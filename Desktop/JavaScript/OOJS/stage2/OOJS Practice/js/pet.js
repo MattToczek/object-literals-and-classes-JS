c166119987a747a4653c7268da08feca57cd8967
Matt. Toczek <toczek.matt@gmail.com>
2019-09-04 11:44:17 +0100

    adding consts using classes

--- Desktop/JavaScript/OOJS/stage2/OOJS Practice/js/pet.js
+++ Desktop/JavaScript/OOJS/stage2/OOJS Practice/js/pet.js
@@ -1,5 +1,12 @@
 class Pet {
-  constructor() {
-
+  constructor(animal, age, breed) {
+    this.animal=animal;
+    this.age=age;
+    this.breed=breed;
   }
 }
+
+const ernie = new Pet('dog', 1, 'Pug');
+const vera = new Pet('dog', 8, 'Border Collie');
+const scofield = new Pet('dog', 6, 'Doberman');
+const edel = new Pet('dog', 7, 'German Shorthaired Pointer');
