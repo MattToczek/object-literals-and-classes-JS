ea5c0b8af1c9260e81db3f229a37c75c3a67274e
Matt. Toczek <toczek.matt@gmail.com>
2019-09-04 11:19:22 +0100

    adding js folder, new js file and updating src in html

---
+++ Desktop/JavaScript/OOJS/stage2/OOJS Practice/js/pets.js
@@ -0,0 +1,75 @@
+const ernie = {
+    animal: 'dog',
+    age: '1',
+    breed: 'pug',
+    bark: function(){
+        console.log('Woof!');
+    }
+}
+
+const vera = {
+    animal: 'dog',
+    age: 8,
+    breed: 'Border Collie',
+    bark: function(){
+        console.log('Woof!');
+    }
+}
+
+const scofield = {
+    animal: 'dog',
+    age: 6,
+    breed: 'Doberman',
+    bark: function(){
+        console.log('Woof!');
+    }
+}
+
+const edel = {
+    animal: 'dog',
+    age: 7,
+    breed: 'German Shothaired Pointer',
+    bark: function(){
+        console.log('Woof!');
+    }
+}
+
+// Dot Notation Usage
+console.log(ernie.age);
+console.log(ernie.breed);
+ernie.bark();
+
+// Bracket Notation Usage
+console.log(ernie['age']);
+console.log(ernie['breed']);
+ernie['bark']();
+
+// Using Brackets and variables to Access properties
+let prop = 'breed';
+console.log(ernie[prop]);
+
+// adding and ammending property values using dot notation
+ernie.age= 2;
+ernie.color = 'black';
+console.log(ernie);
+
+
+// adding and ammending property values using bracket notation
+ernie['age'] = 3;
+ernie['color'] = 'white';
+console.log(ernie);
+
+// accessing string values inside object literals
+const myString = {
+	string: "Strings are easy to manipulate.",
+	countWords: function(){
+		const wordArray = this.string.split(' ');
+		return wordArray.length;
+	}
+}
+
+myString.string = "I manipulated this string using JS."
+
+myString.characters = myString.string.length;
+
+var numWords = myString.countWords();
